534d3d5cc3d171f6849952ce8f06bcb666b40bf0
Marcin Chwedczuk <0xmarcin+dev@gmail.com>
2021-09-04 11:23:46 +0200

    project skel

---
+++ src/main/java/pl/marcinchwedczuk/polishholidays/App.java
@@ -0,0 +1,13 @@
+package pl.marcinchwedczuk.polishholidays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
