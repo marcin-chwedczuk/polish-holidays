4c4be24bce5fb91ff1ecaf392898c9cf9fab7a93
Marcin Chwedczuk <0xmarcin+dev@gmail.com>
2021-11-06 16:01:29 +0100

    More polishing

--- src/main/java/pl/marcinchwedczuk/polishholidays/HolidayDateAlgorithms.java
+++ src/main/java/pl/marcinchwedczuk/polishholidays/HolidayDateAlgorithms.java
@@ -1,12 +1,12 @@
 package pl.marcinchwedczuk.polishholidays;
 
 public class HolidayDateAlgorithms {
-    public static HolidayDateAlgorithm fixedAtMonthDay(int month, int day) {
-        return new FixedDateHolidayDateAlgorithm(month, day);
-    }
+  public static HolidayDateAlgorithm fixedAtMonthDay(int month, int day) {
+    return new FixedDateHolidayDateAlgorithm(month, day);
+  }
 
-    public static HolidayDateAlgorithm relativeToEaster(int offsetDays) {
-        return new RelativeDateHolidayDateAlgorithm(
-                new EasterDateHolidayDateAlgorithm(), offsetDays);
-    }
+  public static HolidayDateAlgorithm relativeToEaster(int offsetDays) {
+    return new RelativeDateHolidayDateAlgorithm(
+        new EasterDateHolidayDateAlgorithm(), offsetDays);
+  }
 }
